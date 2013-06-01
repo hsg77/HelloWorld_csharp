c1d68395bfd02166c1789f47fb4d16586ef61983
hsg77 <hsg77@163.com>
2013-06-02 01:59:09 +0800

    add program note info

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,6 +6,12 @@ namespace HelloWorld
 {
     class Program
     {
+        /// <summary>
+        /// 主程序入口
+        /// vp:hsg
+        /// create date:2013-05
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello World!");
